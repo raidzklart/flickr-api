5bfe3da17f08747602d91a30b400a6246a7cd97b
Thomas Christopher <thomas.j.christopher@gmail.com>
2019-10-21 20:03:24 +0100

    Add bootstrap

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,7 +1,13 @@
 class StaticPagesController < ApplicationController
   def home
-    if params[:id]
-      @photos = Flickr.photos.search(user_id: params[:id])
+    begin
+      if params[:id]
+        @photos = Flickr.photos.search(user_id: params[:id])
+      end
+    rescue => e
+      logger.error e.message
+      flash.now[:error] = "There has been a problem finding the images for that user."
+      render :home
     end
   end
 
